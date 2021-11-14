b09f2ac7ffed5264dc1b6a51fa197e9e1fdf9dd5
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-11-14 00:08:52 -0600

    Update PIA.py
    Co-Authored-By: cantuandrea <92405271+cantuandrea@users.noreply.github.com>
    Co-Authored-By: José Fernando Chávez Castillo <78767493+ImGodsGamerZ@users.noreply.github.com>
    Co-Authored-By: JuanDSanchez2021 <92406360+JuanDSanchez2021@users.noreply.github.com>
    Co-Authored-By: Alejandro López <92610512+OGAlexSchutz@users.noreply.github.com>

--- PIA.py
+++ PIA.py
@@ -11,3 +11,7 @@ separador = ('-' * 45)
 
 print('Bienvenido(a) al negocio de venta de llantas')
 print(separador)
+
+def Menu():
+    opcion = int(input('Menú de opciones:\n[1] Registrar una venta\n[2] Consultar una venta\n[3] Reporte de ventas de fecha\n[4] Salir y guardar en base de datos (.db)\n» '))
+    return opcion
